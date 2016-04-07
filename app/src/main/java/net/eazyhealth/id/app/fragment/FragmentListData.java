5a16804b56ba594237f9d4f6629ab8b76019498f
galihrepo <galihadityo@yahoo.co.id>
2016-04-08 01:40:22 +0700

    memeks

--- app/src/main/java/net/eazyhealth/id/app/fragment/FragmentListData.java
+++ app/src/main/java/net/eazyhealth/id/app/fragment/FragmentListData.java
@@ -6,15 +6,25 @@ import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import com.orhanobut.dialogplus.DialogPlus;
+import com.orhanobut.dialogplus.OnBackPressListener;
+import com.orhanobut.dialogplus.OnCancelListener;
+import com.orhanobut.dialogplus.OnDismissListener;
+import com.orhanobut.dialogplus.OnItemClickListener;
 
 import net.eazyhealth.id.app.R;
 import net.eazyhealth.id.app.adapter.AdapterItemLoadMore;
 import net.eazyhealth.id.app.application.MyApplication;
+import net.eazyhealth.id.app.custom.CustomRippleView;
 import net.eazyhealth.id.app.custom.CustomSwipeRefreshLayout;
 import net.eazyhealth.id.app.custom.CustomToast;
+import net.eazyhealth.id.app.custom.RippleViewAndroidM;
 import net.eazyhealth.id.app.model.response.mcu.McuModel;
 import net.eazyhealth.id.app.rest.EndPoints;
 import net.eazyhealth.id.app.rest.RestHelper;
@@ -28,18 +38,21 @@ import retrofit2.Response;
 /**
  * Created by GALIH ADITYO on 4/7/2016.
  */
-public class FragmentListData extends Fragment implements SwipeRefreshLayout.OnRefreshListener {
+public class FragmentListData extends Fragment implements SwipeRefreshLayout.OnRefreshListener, RippleViewAndroidM.OnRippleCompleteListener {
 
     private CustomSwipeRefreshLayout swipeRefreshLayout;
     private RecyclerView mRecyclerView;
     private AdapterItemLoadMore mAdapter;
     private String nextPage = null;
+    private CustomRippleView rvSort;
+    private CustomRippleView rvFilter;
+    private DialogPlus dialogFilter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_list_data, container, false);
-        initView(view);
+        initView(view, savedInstanceState);
         initVariable();
         return view;
     }
@@ -54,7 +67,15 @@ public class FragmentListData extends Fragment implements SwipeRefreshLayout.OnR
         });
     }
 
-    private void initView(View view) {
+    private void initView(View view, Bundle savedInstanceState) {
+        initDialogFilter(savedInstanceState);
+
+        rvFilter = (CustomRippleView) view.findViewById(R.id.rv_filter);
+        rvFilter.setOnRippleCompleteListener(this);
+
+        rvSort = (CustomRippleView) view.findViewById(R.id.rv_sort);
+        rvSort.setOnRippleCompleteListener(this);
+
         mRecyclerView = (RecyclerView) view.findViewById(R.id.rv);
         swipeRefreshLayout = (CustomSwipeRefreshLayout) view.findViewById(R.id.swipe_refresh);
         swipeRefreshLayout.setOnRefreshListener(this);
@@ -62,6 +83,24 @@ public class FragmentListData extends Fragment implements SwipeRefreshLayout.OnR
         // recycleview
         mRecyclerView.setHasFixedSize(true);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    private void initDialogFilter(Bundle savedInstanceState) {
+        dialogFilter = DialogPlus.newDialog(getActivity())
+                .setAdapter(new ArrayAdapter<>(getActivity(), R.layout.content_dialog, new String[]{"Klinik Jantung", "Klinik Mata", "Klinik Hati"}))
+                .setCancelable(true)
+                .setGravity(Gravity.TOP)
+                .setContentBackgroundResource(R.color.colorPrimaryDark)
+                .setInAnimation(R.anim.slide_in_top)
+                .setOutAnimation(R.anim.slide_out_top)
+                .setHeader(R.layout.header_dialog)
+                .setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(DialogPlus dialog, Object item, View view, int position) {
+                        CustomToast.setMessage(getActivity(), position+"");
+                    }
+                })
+                .create();
     }
 
     @Override
@@ -160,5 +199,14 @@ public class FragmentListData extends Fragment implements SwipeRefreshLayout.OnR
                 mAdapter.setLoaded();
             }
         });
+    }
+
+    @Override
+    public void onComplete(RippleViewAndroidM rippleView) {
+        if (rippleView == rvFilter) {
+            dialogFilter.show();
+        } else if (rippleView == rvSort) {
+
+        }
     }
 }
